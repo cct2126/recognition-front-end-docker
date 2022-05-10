963324218d9bdc6da174391faecdf5d232161b8f
cctsui <84331653+cct2126@users.noreply.github.com>
2022-05-10 17:32:35 +0800

    db connection

--- flask1.py
+++ flask1.py
@@ -1,11 +1,18 @@
+
+
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import requests
+import redis
+import json
 
 import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'upload/'
+
+# r = redis.Redis(host='112.74.161.101', port=6379)
+r = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
@@ -19,12 +26,25 @@ def upload_file():
 def uploader():
     data = request.data
     url = 'http://recognition:5678/recog'
+    # url = 'http://localhost:5678/recog'
 
     myobj = data
 
     x = requests.post(url, data=myobj)
-    print(x.text)
+    result_json = json.loads(x.text)
+    result = result_json['result']
+    print(result)
+
+    import uuid
+    uuid = uuid.uuid1()
+
+    payload = str({result, myobj})
+    # print(payload)
+    r.set(int(uuid), payload)
+
+    # print(r.get(int(uuid)))
+
     return x.text
 
 if __name__ == '__main__':
-   app.run(debug=True, host = '0.0.0.0', port = 5100)
+    app.run(debug=True, host = '0.0.0.0', port = 5100)
---
+++ redis-function.py
@@ -0,0 +1,7 @@
+import redis
+
+r = redis.Redis(host='112.74.161.101', port=32003, db=0)
+print(r.keys())
+for elem in r.keys():
+    r.delete(elem)
+print(r.keys())
