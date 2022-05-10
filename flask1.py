fa9344d69fc731b1217b3d73a5189239c3d8a7bb
cctsui <84331653+cct2126@users.noreply.github.com>
2022-05-11 01:15:58 +0800

    show images init

--- flask1.py
+++ flask1.py
@@ -1,5 +1,4 @@
 
-
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import requests
@@ -12,22 +11,22 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'upload/'
 app.static_folder = 'templates/static'
 
-# r = redis.Redis(host='112.74.161.101', port=32003)
-r = redis.Redis(host='redis', port=6379)
+r = redis.Redis(host='112.74.161.101', port=32003)
+# r = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
     return 'hello docker&flask'
 
-@app.route('/upload',methods=['GET'])
+@app.route('/upload' ,methods=['GET'])
 def upload_file():
     return render_template('upload.html')
 
-@app.route('/uploader',methods=['POST'])
+@app.route('/uploader' ,methods=['POST'])
 def uploader():
     data = request.data
-    url = 'http://recognition:5678/recog'
-    # url = 'http://localhost:5678/recog'
+    # url = 'http://recognition:5678/recog'
+    url = 'http://localhost:5678/recog'
 
     myobj = data
 
@@ -39,10 +38,35 @@ def uploader():
     import uuid
     uuid = uuid.uuid1()
 
-    payload = str({"result": result, "object": myobj})
+    import base64
+    base64_str = base64.b64encode(myobj).decode("ascii")
+    base64_str = 'data:image/jpeg;base64,' + base64_str
+
+    ## payload = str({"result": result, "object": str(myobj)})
+    print(base64_str)
+    payload = str("result={};object={}".format(result, base64_str))
     r.set(int(uuid), payload)
 
     return x.text
+
+@app.route('/get_display_image', methods=['GET'])
+def get_display_image():
+    img_keys = r.keys()[-8:]
+    img_list = {}
+    i = 0
+    for elem in img_keys:
+        tmp = (r.get(elem)).decode('utf-8')
+
+        label = tmp.split("=")[1].split(";")[0]
+        data = tmp.split("object=")[1]
+
+        payload = {"label": label, "data": data}
+        img_list[i] = payload
+
+        i += 1
+
+        # print(img_list)
+    return img_list
 
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0', port = 5100)
--- redis-function.py
+++ redis-function.py
@@ -1,3 +1,5 @@
+import json
+
 import redis
 
 r = redis.Redis(host='112.74.161.101', port=32003, db=0)
@@ -7,5 +9,19 @@ print(r.keys())
 # print(r.keys())
 temp = r.keys()
 for elem in temp:
-    r.get(elem)
-    print("emm")
+
+    tmp = (r.get(elem)).decode('utf-8')
+
+    label = tmp.split("=")[1].split(";")[0]
+    data = tmp.split("b'")[1]
+
+    print(label)
+    print(data.encode('utf-8'))
+
+
+
+    # tmp = json.loads(tmp)
+    # print(tmp)
+    # print(tmp['result'])
+
+
